38719791143d13ceee4f30843e4917e12230aa64
RajenderMohan <rajendramohan7800@gmail.com>
2025-08-04 10:33:21 +0530

    Commit 5

--- OrderEats/app/src/main/java/com/rajender/ordereats/Fragment/HomeFragment.kt
+++ OrderEats/app/src/main/java/com/rajender/ordereats/Fragment/HomeFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.rajender.ordereats.R
+import com.rajender.ordereats.adapter.PopulerAdapter
 import com.rajender.ordereats.databinding.FragmentHomeBinding
 
 @Suppress("UNREACHABLE_CODE")
@@ -41,6 +45,25 @@ class HomeFragment : Fragment() {
         val imageSlider = binding.imageSlider
         imageSlider.setImageList(imageList)
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
+        imageSlider.setItemClickListener(object  : ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMassage = "Selected Image $position"
+                Toast.makeText(requireContext(), itemMassage,Toast.LENGTH_SHORT).show()
+            }
+        })
+        val foodName = listOf("Burger","Sandwich","Pizza","Momos", "Burger","Sandwich","Pizza")
+        val Price = listOf("$5","$6","$7","$88","$5","$6","$77")
+        val populerFoodImages =
+            listOf(R.drawable.burger, R.drawable.logo_app1, R.drawable.logo_app, R.drawable.logo_app1, R.drawable.burger, R.drawable.logo_app, R.drawable.logo_app1,)
+        val adapter = PopulerAdapter(foodName, Price, populerFoodImages)
+        binding.PopulerRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.PopulerRecyclerView.adapter = adapter
+
     }
 
     companion object{
--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Create app - OrderEats
 4. activity - LoginActivity
 5. activity - SignActivity
 6. activity - ChooseLocationActivity
+7. activity - populer_item
+8. activity - cart_item
 
 <!-- Create Fragment Files -->
 
@@ -19,11 +21,19 @@ Create app - OrderEats
     HistoryFragment
     ProfileFragment
 
+<!-- Create Adapter Files -->
+
+    PopulerAdapter
+    CartAdapter
+
 <!-- Create Drawable Files -->
 
     whitwButton.xml
     edittextshape.xml
     greenbuttongradient.xml
+    addtocart.xml
+    textviewshape
+    proceedbuttonshape
 
 <!-- Create Navigation File -->
 
@@ -34,6 +44,6 @@ Create app - OrderEats
     fade_in.xml
     slide_up_button.xml
     fade_in_slide_down.xml
-     slide_in_from_left.xml
+    slide_in_from_left.xml
     slide_in_from_right.xml
     fade_in_slide_up_delayed.xml
